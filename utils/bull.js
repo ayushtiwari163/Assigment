522e568ed2ae38a95aaf51e8e02d88030cd39ada
Ayush Tiwari <ayushtiwari163@gmail.com>
2025-03-01 20:39:11 +0530

    added redis pass in bull

--- utils/bull.js
+++ utils/bull.js
@@ -1,7 +1,7 @@
 const Queue = require("bull");
 const {redisConfig} = require('../config')
 const redisOptions = {
-  connection: { host: redisConfig.host, port: redisConfig.port },
+  connection: { host: redisConfig.host, port: redisConfig.port, auth_pass: redisConfig.auth_pass },
 };
 
 const csvProcessingQueue = new Queue("csvProcessingQueue", redisOptions);
